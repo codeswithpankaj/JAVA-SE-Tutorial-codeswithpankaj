012d340e4d3b197bc2ea0ccc326539ac1fc15fda
p4n.in <143927285+codeswithpankaj@users.noreply.github.com>
2024-08-22 02:14:23 +0000

    update Operator

---
+++ Day_02_Operators/Operators/Operator_Example.java
@@ -0,0 +1,21 @@
+package Day_02_Operators.Operators;
+/**
+ * Operator_Example
+ */
+public class Operator_Example {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(56+45);
+        System.out.println(56*45);
+        System.out.println(56/45);
+        System.out.println(56-45);
+        System.out.println(56%45);
+
+
+    }
+
+
+
+}
